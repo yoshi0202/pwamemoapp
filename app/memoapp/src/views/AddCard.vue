e4ef1a61e7f6acf8585b98de2b697616833071fa
“yoshi_0202“ <kobayashi020202@gmail.com>
2020-01-03 15:37:58 +0900

    add title & tags

--- app/memoapp/src/views/AddCard.vue
+++ app/memoapp/src/views/AddCard.vue
@@ -1,12 +1,27 @@
 <template>
-  <v-container fluid>
+  <v-container fill-height fluid py-0>
     <v-layout text-center wrap>
       <v-flex md12>
-        <mavon-editor language="ja" v-model="cardContents" />
+        <v-container fluid px-0>
+          <v-card elevation="0">
+            <v-form>
+              <v-container pt-3 pb-0 fluid>
+                <v-text-field label="カードタイトル"></v-text-field>
+              </v-container>
+              <v-container pt-2 fluid>
+                <v-text-field label="タグ"></v-text-field>
+              </v-container>
+            </v-form>
+          </v-card>
+        </v-container>
+
+        <mavon-editor language="ja" v-model="cardContents" placeholder="カード内容を入力" />
+        <v-container mt-2>
+          <v-btn @click="test">submitTest</v-btn>
+        </v-container>
+        <div v-html="parseContents"></div>
       </v-flex>
     </v-layout>
-    <v-btn @click="test">submitTest</v-btn>
-    <div v-html="parseContents"></div>
   </v-container>
 </template>
 
@@ -28,4 +43,8 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+.markdown-body {
+  height: 62%;
+}
+</style>
