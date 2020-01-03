7d9d7a5c5ccfd24dc829e3283334416af51b35c0
“yoshi_0202“ <kobayashi020202@gmail.com>
2020-01-03 11:59:40 +0900

    change header & delete about page

---
+++ app/memoapp/src/views/AddCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-layout text-center wrap>
+      <v-flex md12 class="red">aiueo</v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "addCard"
+};
+</script>
+
+<style></style>
