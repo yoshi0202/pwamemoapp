7557cb47072b2271a4ef20dd9872c6823c521d99
“yoshi_0202“ <kobayashi020202@gmail.com>
2020-01-03 14:49:41 +0900

    add marked

--- app/memoapp/src/views/AddCard.vue
+++ app/memoapp/src/views/AddCard.vue
@@ -2,15 +2,29 @@
   <v-container fluid>
     <v-layout text-center wrap>
       <v-flex md12>
-        <mavon-editor language="ja" />
+        <mavon-editor language="ja" v-model="cardContents" />
       </v-flex>
     </v-layout>
+    <v-btn @click="test">submitTest</v-btn>
+    <div v-html="parseContents"></div>
   </v-container>
 </template>
 
 <script>
+import marked from "marked";
 export default {
-  name: "addCard"
+  name: "addCard",
+  data: function() {
+    return {
+      cardContents: "",
+      parseContents: ""
+    };
+  },
+  methods: {
+    test: function() {
+      this.parseContents = marked(this.cardContents);
+    }
+  }
 };
 </script>
 
