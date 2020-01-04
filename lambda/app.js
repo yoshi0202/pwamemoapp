3c7d39a80c63b30305a5917dc613276d7d7d4d08
“yoshi_0202“ <kobayashi020202@gmail.com>
2020-01-04 10:38:14 +0900

    add serverless files

--- lambda/app.js
+++ lambda/app.js
@@ -9,10 +9,6 @@ var apiRouter = require("./routes/api");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -21,6 +17,11 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors());
 app.use("/api", apiRouter);
+app.get("/", function(req, res, next) {
+  res.json({
+    status: "ok"
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
