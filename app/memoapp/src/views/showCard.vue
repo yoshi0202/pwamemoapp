7fbf3d2dc68a54a32f59edc1b080cb938af575a0
“yoshi_0202“ <kobayashi020202@gmail.com>
2020-01-07 07:38:23 +0900

    show tags

--- app/memoapp/src/views/showCard.vue
+++ app/memoapp/src/views/showCard.vue
@@ -2,11 +2,24 @@
   <v-container fluid pa-0 ma-0>
     <v-layout>
       <v-flex text-center>
-        <v-container text-left title>{{ cardData.cardData.title }}</v-container>
+        <v-container text-left title>
+          {{ cardData.cardData.title }}
+          <v-divider></v-divider>
+        </v-container>
+        <v-container text-left>
+          <v-layout>
+            <v-flex>
+              <v-chip v-for="tag in cardData.cardData.tags" :key="tag" small>{{ tag }}</v-chip>
+            </v-flex>
+          </v-layout>
+        </v-container>
         <v-container>
           <v-card>
             <v-container v-html="parseMd(cardData.cardData.contents)" text-left></v-container>
           </v-card>
+        </v-container>
+        <v-container>
+          <v-divider></v-divider>
         </v-container>
       </v-flex>
     </v-layout>
@@ -22,7 +35,9 @@ export default {
   mounted: async function() {
     const userId = this.$route.params.user;
     const cardId = this.$route.params.cardid;
-    const result = await axios.get("http://localhost:3000/api/" + userId + "/cards/" + cardId);
+    const result = await axios.get(
+      "https://u65qbs6yva.execute-api.ap-northeast-1.amazonaws.com/prod/api/" + userId + "/cards/" + cardId
+    );
     this.cardData = result.data.Item;
     console.log(marked(JSON.stringify(this.cardData)));
   },
