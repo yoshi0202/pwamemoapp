eadff75fb7d5aaa53ad52ee237d88eb3a17f03a7
“yoshi_0202“ <kobayashi020202@gmail.com>
2020-01-08 22:35:30 +0900

    add fromt login form

--- app/memoapp/src/router/index.js
+++ app/memoapp/src/router/index.js
@@ -3,15 +3,11 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import AddCard from "../views/AddCard.vue";
 import showCard from "../views/showCard.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "home",
-    component: Home
-  },
   {
     path: "/addCard",
     name: "addCard",
@@ -21,6 +17,16 @@ const routes = [
     path: "/:user/card/:cardid",
     name: "showCard",
     component: showCard
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
+  },
+  {
+    path: "*",
+    name: "home",
+    component: Home
   }
 ];
 
---
+++ app/memoapp/src/views/Login.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-container fluid pa-0 ma-0 fill-height>
+    <v-layout align-center>
+      <v-flex md6 offset-md3>
+        <v-card>
+          <v-container text-center headline>Snippy</v-container>
+          <v-container text-center fluid>
+            <v-form ref="form">
+              <v-text-field v-model="email" label="Email"></v-text-field>
+              <v-text-field v-model="password" label="Password" type="password"></v-text-field>
+              <v-layout>
+                <v-flex md-6>
+                  <v-btn large color="primary" @click="alertFunc">Login</v-btn>
+                </v-flex>
+                <v-flex md-6>
+                  <v-btn large color="secondary">SignUP</v-btn>
+                </v-flex>
+              </v-layout>
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  mounted: async function() {},
+  data: function() {
+    return {
+      email: "",
+      password: ""
+    };
+  },
+  created: function() {},
+  methods: {
+    alertFunc: function() {
+      alert(this.email);
+      alert(this.password);
+    }
+  }
+};
+</script>
+
+<style></style>
